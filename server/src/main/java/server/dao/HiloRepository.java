c1098d6b14c7befb56def4dc81e9eaf77bf06836
JurgenEnsunza <jurgen.ensunza@opendeusto.es>
2026-03-24 13:40:26 +0100

    Implementación Backend busqueda de hilos (SERVER). -Añadir metodo BusquedaHilo en HiloRespository -Crear HiloService con logica de busqueda -Crear HiloController con endpoint api/hilos/search para hacer GET

--- server/src/main/java/server/dao/HiloRepository.java
+++ server/src/main/java/server/dao/HiloRepository.java
@@ -3,8 +3,14 @@ package server.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import server.entity.Hilo;
+import java.util.List;
 
 @Repository
 public interface HiloRepository extends JpaRepository<Hilo, Integer> {
+
+    List<Hilo> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
+            String title, String description
+    );
+
 
 }
